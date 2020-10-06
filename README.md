44cd429e84368835c8ae85bacd8c1a5062ea6255
Mohammad Yaghobi <m.yaghobi.abc@gmail.com>
2020-10-06 12:55:25 +0330

    v1.1.0
    Now you can make your checkpoints.
    README.md & screenshots updated.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # F3-Benchmark
  A benchmark helper plugin for Fat-Free framework.
 
-![screenshot](screenshot/shot.PNG?raw=true "F3 Benchmark Screentshot")
+![screenshot](screenshots/shot1.jpg?raw=true "F3 Benchmark Screentshot")
+![screenshot-full](screenshots/shot2.jpg?raw=true "F3 Benchmark Screentshot Full")
 
 ### Install
 
@@ -9,14 +10,27 @@ Just copy `benchmark.php` into your `lib/` folder.
 
 ### Usage
 
-This plugin will work if `DEBUG>=3`, so just initiate it after your config file or after `$f3->set('DEBUG', YOUR_DEBUG_LEVEL);`.
+Benchmark just works if `DEBUG>=3`, otherwise, it will disable to get not resource usage.
+Initiate `Benchmark` after your config file:
 
 ``` php
 $f3->config('config.ini');
 Benchmark::instance();
 ...
 ```
-
+or
+``` php
+$f3->set('DEBUG', YOUR_DEBUG_LEVEL);
+Benchmark::instance();
+```
+Then you can make your checkpoint:
+``` php
+$f3->get('benchmark')->checkpoint('myTag');
+```
+or
+``` php
+$f3->benchmark->checkpoint();
+```
 ## License
 
 You are allowed to use this plugin under the terms of the GNU General Public License version 3 or later.
---
+++ lib/benchmark.php
@@ -0,0 +1,271 @@
+<?php
+
+/**
+ * @package F3 Benchmark
+ * @version 1.1.0
+ * @link http://github.com/myaghobi/F3-Benchmark Github
+ * @author Mohammad Yaghobi <m.yaghobi.abc@gmail.com>
+ * @copyright Copyright (c) 2020, Mohammad Yaghobi
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3
+ */
+
+class Benchmark extends \Prefab {
+  private $checkPoints;
+  private $ramUsage;
+  private $ramUsagePeak;
+
+  /**
+   * these properties will help us to save performance
+   * imagine to have hundreds of checkpoints, so we don't need to use count() on that big array
+   * or there will be no memory usage if you are not in DEBUG mode
+   */
+  private $isBenchmarkEnable;
+  private $lastCheckPointInMS;
+  private $lastCheckPointNumber;
+
+  /**
+   * Benchmark constructor
+   *
+   * @return void
+   */
+  function __construct() {
+    $this->isBenchmarkEnable = false;
+
+    $f3 = \Base::instance();
+    if ($f3->get('DEBUG') >= 3) {
+      $this->isBenchmarkEnable = true;
+      $this->checkPoints = array();
+      $this->ramUsage = array();
+      $this->ramUsagePeak = 0;
+      $this->lastCheckPointInMS = 0;
+      $this->lastCheckPointNumber = 0;
+
+      // add Start point, $this->lastCheckPointInMS == 0
+      $this->checkPoint('Start');
+      // add Benchmark Init point, $this->lastCheckPointInMS > 0
+      $this->checkPoint('Benchmark Init');
+
+      register_shutdown_function(function () {
+        $this->enhanceExcutionTime();
+        $this->showBenchmark();
+      });
+    }
+
+    // you can comment below lines if you don't need to checkPoint()
+    $f3->set('benchmark', $this);
+
+    // you can comment below line if you don't like it
+    $f3->benchmark = $this;
+  }
+
+
+  /**
+   * add a new checkpoint to checkpoints array & add to ram usage array
+   * 
+   * @return void
+   */
+  public function checkPoint($tag = '') {
+    if (!$this->isBenchmarkEnable) {
+      return;
+    }
+
+    $currentTime = $this->getCurrentTime();
+    $ramUsagePeak = $this->getRamUsagePeak();
+
+    if (empty($tag)) {
+      $tag = 'Check Point ' . ($this->lastCheckPointNumber+1);
+    }
+
+    if ($this->lastCheckPointInMS == 0) {
+      $currentTime = $this->getRequestTime();
+    }
+
+    $this->checkPoints[$tag] = $currentTime;
+    $this->ramUsage[$tag] = $ramUsagePeak;
+    $this->ramUsagePeak = max($ramUsagePeak, $this->ramUsagePeak);
+
+    $this->lastCheckPointInMS = $currentTime;
+    $this->lastCheckPointNumber += 1;
+  }
+
+    
+  /**
+   * calculate excution time for each checkpoint
+   *
+   * @return void
+   */
+  public function enhanceExcutionTime() {
+    $prevKey ='';
+    $prevTime =0;
+
+    foreach ($this->checkPoints as $key => $value) {
+      if ($prevTime==0) {
+        $prevKey=$key;
+        $prevTime=$value;
+        continue;
+      }
+      $this->checkPoints[$prevKey] = $value-$prevTime;
+      
+      $prevKey=$key;
+      $prevTime=$value;
+    }
+    
+    $this->checkPoints[$prevKey] = $this->getCurrentTime()-$prevTime;
+  }
+
+    
+  /**
+   * get real ram usage
+   *
+   * @return int
+   */
+  public function getRamUsagePeak() {
+    // true => memory_real_usage
+    return memory_get_peak_usage(true);
+  }
+
+
+  /**
+   * get elapsed times from beginning till now
+   *
+   * @return int
+   */
+  public function getExecutionTime() {
+    return $this->getCurrentTime()-$this->getRequestTime();
+  }
+
+    
+  /**
+   * get request time in milliseconds
+   *
+   * @return int
+   */
+  public function getRequestTime() {
+    return round($_SERVER["REQUEST_TIME_FLOAT"] * 1000);
+  }
+
+
+  /**
+   * get the count of added points
+   *
+   * @return int
+   */
+  public function getCheckPointsCount() {
+    return count($this->checkPoints);
+  }
+
+
+  /**
+   * clear checkpoints & ram usages
+   *
+   * @return void
+   */
+  public function clear() {
+    $this->checkPoints = array();
+    $this->ramUsage = array();
+  }
+
+
+  /**
+   * get current time in milliseconds
+   *
+   * @return int
+   */
+  public function getCurrentTime() {
+    $microtime = microtime(true) * 1000;
+    return round($microtime);
+  }
+
+
+  /**
+   * format bytes with kB, mB, etc. as mentioned in https://en.wikipedia.org/wiki/KB
+   *
+   * @param  int $size
+   * @return string
+   */
+  public function getFormattedBytes($size) {
+    $base = log($size, 1024);
+    $suffixes = array('', 'kB', 'mB', 'gB', 'tB');
+
+    return round(pow(1024, $base - floor($base)), 1) . ' ' . $suffixes[floor($base)];
+  }
+
+  /**
+   * generate a log of checkpoints & ram usage
+   *
+   * @return void
+   */
+  public function getDetailsLog($lineDelimiter="<br>") {
+    $str = '';
+    foreach ($this->checkPoints as $key => $value) {
+      $str .= "$key => Time: <b>$value ms</b>, Memory: <b>" . $this->getFormattedBytes($this->ramUsage[$key]) . "</b>".$lineDelimiter;
+    }
+    return $str;
+  }
+
+
+  /**
+   * print formatted benchmark log
+   *
+   * @return void
+   */
+  public function showBenchmark() {
+    print '<div class="benchmark-panel" class="benchmark-panel-main">' .
+      'Time: <b>' . $this->getExecutionTime() . ' ms</b>, ' .
+      'Memory: <b>' . $this->getFormattedBytes($this->ramUsagePeak) . '</b>, ' .
+      'Points: <b>' . $this->lastCheckPointNumber . '</b>' .
+      '<a href="javascript: void" id="benchmark-toggle">&#8661;</a>'.
+      '</div>';
+
+    print '<div class="benchmark-panel" id="benchmark-panel-log" style="display:none">' .
+      $this->getDetailsLog();
+      '</div>';
+
+    print '<script>
+        document.getElementById("benchmark-toggle").onclick = function () {
+          var x = document.getElementById("benchmark-panel-log");
+          if (x.style.display == "none") {
+              x.style.display = "inline-block";
+          } else {
+              x.style.display = "none";
+          }
+          return false;
+        };
+      </script>';
+
+    print '<style>
+      .benchmark-panel {
+        position: fixed!important;
+        font-family: tahoma!important;
+        z-index: 1000000!important;
+        bottom: 5px!important;
+        left: 5px!important;
+        direction: ltr!important;
+        text-align: left!important;
+        border: 1px solid #dee2e6!important;
+        border-radius: 5px!important;
+        font-size: 13px!important;
+        background: #f8f9fa!important;
+        color: #555!important;
+        padding: 7px 10px!important;
+      }
+      #benchmark-panel-log {
+        bottom: 45px!important;
+      }
+      .benchmark a {
+        color: #555;
+        text-decoration: underline;
+      }
+      .benchmark b {
+        color: #333;
+      }
+      #benchmark-toggle {
+        display:inline-block;
+        font-size: 13px;
+        padding: 0px 3px 0 1px;
+        margin-top: -1px;
+        vertical-align: top;
+      }
+    </style>';
+  }
+}
